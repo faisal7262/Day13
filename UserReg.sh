1ba9c269fa42159078834de7402e7a71ac56a029
“Enter <shaikhfaisal6342@gmail.com>
2020-10-14 16:46:02 +0530

    email  uc3

--- UserReg.sh
+++ UserReg.sh
@@ -19,3 +19,12 @@ else
 	echo false
 fi
 
+#uc 3
+read email 
+pattern3="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+
+ if [[ $email =~ $pattern3 ]]; then
+	echo true 
+else 
+echo false
+ fi
